ae3b89c692266958231f73d9f584095fd432efec
Yunus Emre <akgunyunus482@gmail.com>
2021-01-05 15:23:59 +0300

    App.js file update

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React ,{useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [name,setName]=useState('Yunus');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.part1}>
+        <Text style={styles.part2}> Burası Part 1 içindir.</Text>
+      </View>
+      <View style={styles.body}>
+        <Text> Burada da body kısmı var </Text>
+        <Text> Benim adım {name} </Text>
+        <Text> Burada da body kısmı var </Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +26,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  part1 :{
+    backgroundColor: '#fa3'
+  },
+  part2 :{
+    fontSize: 18,
+
+  },
+  body :{
+    marginTop:10 ,
+    padding:20,
+    backgroundColor:  '#3a2'
+  }
 });
